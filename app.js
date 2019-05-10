35b2476430ba4d3296b00e2a53f1eb6158a88201
owengiri20 <46738862+owengiri20@users.noreply.github.com>
2019-05-11 00:49:53 +0800

    reading DOM input data

--- app.js
+++ app.js
@@ -6,24 +6,46 @@ var budgetController = (function() {
 
 // UI controller
 var UIController = (function() {
+  var DOMStrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn"
+  };
   // Some code...
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMStrings.inputType).value, // will be inc or exp
+        description: document.querySelector(DOMStrings.inputDescription).value,
+        value: document.querySelector(DOMStrings.inputValue).value
+      };
+    },
+
+    getDOMStrings: function() {
+      return DOMStrings;
+    }
+  };
 })();
 
 // bridge between uiController and budgetController
 // global app controller
 var controller = (function(budgetCtrl, UICtrl) {
   // some code...
-
+  var DOM = UICtrl.getDOMStrings();
   var ctrlAddItem = function() {
     // get field input data
+    var input = UICtrl.getInput();
+
+    console.log(input);
+
     // add item to the budget controller
     // add item to the UI
     // calculate the budget
     // display the budget on the UI
-    console.log("lolo");
   };
 
-  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
   document.addEventListener("keypress", function(event) {
     if (event.keyCode === 13 || event.which === 13) {
       ctrlAddItem();
